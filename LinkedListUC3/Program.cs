13318ebde5b9be7cbcf0317e4587168f04e35281
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2021-11-27 15:29:56 +0530

    UC3

---
+++ LinkedListUC3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedListUC3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> l1 = new List<int>();
+            l1.Add(56);
+            l1.Add(30);
+            l1.Add(70);
+
+            foreach (var ele in l1)
+            {
+                Console.WriteLine(ele);
+            }
+
+        }
+    }
+}
