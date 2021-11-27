0f4022ea7c3a1f861b5fc224078faeaaaf77c1e2
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2021-11-27 15:36:45 +0530

    UC8

---
+++ LinkedListUC8/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedListUC8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> L1 = new List<int>();
+            L1.Add(56);
+            L1.Add(30);
+            L1.Add(70);
+
+            foreach (var element in L1)
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.WriteLine("----------------");
+            int size = L1.Count;
+            for (int i = 0; i < size; i++)
+            {
+                if (L1[i] == 30)
+                {
+                    L1.Insert(i + 1, 40);
+                    break;
+                }
+
+            }
+
+            foreach (var element in L1)
+            {
+                Console.WriteLine(element);
+            }
+
+
+
+        }
+    }
+}
