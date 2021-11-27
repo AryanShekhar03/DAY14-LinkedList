7dc3e5a310044094387a892c7fa54fa078cba387
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2021-11-27 15:31:01 +0530

    UC4

---
+++ LinkedListUC4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedListUC4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> l1 = new List<int>();
+            l1.Add(56);
+            l1.Add(70);
+            foreach (var ele in l1)
+            {
+                Console.WriteLine(ele);
+            }
+            l1.Insert(1, 30);
+            Console.WriteLine("After inserting 30 between 56 and 70-------------------------");
+            foreach (var ele in l1)
+            {
+                Console.WriteLine(ele);
+            }
+
+
+
+        }
+    }
+}
