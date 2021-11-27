8444f787380ce5ca06b22f7c097dfff4dbd66863
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2021-11-27 15:27:18 +0530

    UC2

---
+++ LinkedListUC2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedListUC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> l1 = new List<int>();
+            l1.Add(70);
+            l1.Add(30);
+            l1.Add(56);
+            foreach (var ele in l1)
+            {
+                Console.WriteLine(ele);
+            }
+        }
+    }
+}
