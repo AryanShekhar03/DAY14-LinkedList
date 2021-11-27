b5bb79724ccb695c7cbc5b4c73dd1dd1b04fa475
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2021-11-27 15:37:36 +0530

    UC9

---
+++ LinkedListUC9/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedListUC9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> L1 = new List<int>();
+            L1.Add(56);
+            L1.Add(30);
+            L1.Add(40);
+            L1.Add(70);
+
+            foreach (var element in L1)
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.WriteLine("----------After----");
+            int size = L1.Count;
+            for (int i = 0; i < size; i++)
+            {
+                if (L1[i] == 40) ;
+                {
+                    L1.RemoveAt(i);
+                    break;
+                }
+
+            }
+
+            Console.WriteLine("Size is " + L1.Count);
+            foreach (var element in L1)
+            {
+                Console.WriteLine(element);
+            }
+
+        }
+    }
+}
