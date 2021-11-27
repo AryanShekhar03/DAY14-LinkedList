d80c49b026eef9fcc9e3ada0c0f012ca3af157d0
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2021-11-27 15:33:58 +0530

    UC7

---
+++ LinkedListUC7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedListUC7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> L1 = new List<int>();
+            L1.Add(56);
+            L1.Add(30);
+            L1.Add(70);
+            L1.Add(99);
+
+            Console.WriteLine(L1.Contains(30));
+
+        }
+    }
+}
